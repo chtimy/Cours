20a123b0563c2016baa06f6d0df0de69355eb12c
riffet jérémy <jeremy.riffet@gmail.com>
2022-01-21 12:35:43 +0100

    technical update

--- .internal/style_resolver.py
+++ .internal/style_resolver.py
@@ -1,6 +1,10 @@
 styles={'%div%' : 'class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px"',
         '%p%'   : 'class="w3-justify w3-large"',
         '%img%' : 'style="max-width:800px; width:100%; height:60%;"',
+        '%img60%' : 'style="max-width:800px; width:60%;"',
+        '%img50%' : 'style="max-width:800px; width:50%;"',
+        '%img40%' : 'style="max-width:800px; width:40%;"',
+        '%img30%' : 'style="max-width:800px; width:30%;"',
         '%ul%'  : 'class="w3-large" style="text-align: left;"'}
 def resolve_styles(content):
     for key, value in styles.items():
