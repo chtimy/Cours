3a3409165bbcc674a4523b77c560c0a67a2ac497
riffet jérémy <jeremy.riffet@gmail.com>
2022-02-17 23:39:20 +0100

    Up fiche cours 3eme fiche sur les pointeurs

--- .internal/style_resolver.py
+++ .internal/style_resolver.py
@@ -1,6 +1,7 @@
 styles={'%div%' : 'class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px"',
         '%p%'   : 'class="w3-justify w3-large"',
         '%img%' : 'style="max-width:800px; width:100%; height:60%;"',
+        '%img70%' : 'style="max-width:800px; width:70%;"',
         '%img60%' : 'style="max-width:800px; width:60%;"',
         '%img50%' : 'style="max-width:800px; width:50%;"',
         '%img40%' : 'style="max-width:800px; width:40%;"',
